89774467e1242de144250184d670817ba528f735
Shivam Bavaria <shivam.bavaria@gmail.com>
2025-11-30 23:31:03 -0500

    adding presenter for stock search

--- src/main/java/interface_adapter/stock_search/StockSearchPresenter.java
+++ src/main/java/interface_adapter/stock_search/StockSearchPresenter.java
@@ -1,4 +1,37 @@
 package interface_adapter.stock_search;
 
-public class StockSearchPresenter {
+import use_case.stock_search.StockSearchOutputBoundary;
+import use_case.stock_search.StockSearchResponseModel;
+
+public class StockSearchPresenter implements StockSearchOutputBoundary {
+
+    private final StockSearchViewModel viewModel;
+
+    public StockSearchPresenter(StockSearchViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(StockSearchResponseModel responseModel) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(responseModel.getSymbol()).append("\n");
+        sb.append("Current: ").append(responseModel.getCurrentPrice()).append("\n");
+        sb.append("Open:    ").append(responseModel.getOpen()).append("\n");
+        sb.append("High:    ").append(responseModel.getHigh()).append("\n");
+        sb.append("Low:     ").append(responseModel.getLow()).append("\n");
+        sb.append("Prev:    ").append(responseModel.getPreviousClose()).append("\n");
+
+        viewModel.setInfoText(sb.toString());
+        viewModel.setErrorMessage(null);
+        viewModel.setLoading(false);
+        viewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        viewModel.setInfoText(null);
+        viewModel.setErrorMessage(errorMessage);
+        viewModel.setLoading(false);
+        viewModel.firePropertyChanged();
+    }
 }
