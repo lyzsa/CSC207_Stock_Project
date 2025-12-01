5c132ea6110a8c13a7a210250a55b560a5fd87b8
Shivam Bavaria <shivam.bavaria@gmail.com>
2025-11-30 23:20:26 -0500

    request model code

--- src/main/java/use_case/stock_search/StockSearchRequestModel.java
+++ src/main/java/use_case/stock_search/StockSearchRequestModel.java
@@ -1,4 +1,13 @@
 package use_case.stock_search;
 
 public class StockSearchRequestModel {
+    private final String symbol;
+
+    public StockSearchRequestModel(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
